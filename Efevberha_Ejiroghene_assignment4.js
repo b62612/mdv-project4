9c31009c1be1ad032750e9a834ece27faa1950d8
Ejiroghene <ejay.efe@gmail.com>
2013-08-29 16:36:19 -0400

    create function to check string for email format

--- Efevberha_Ejiroghene_assignment4.js
+++ Efevberha_Ejiroghene_assignment4.js
@@ -6,19 +6,20 @@ alert("JavaScript works!");//alert("JavaScript works!");
 // My Function Library
 
 var myFunctionLibrary = function() {
-
+					// try switch statement
 					// To check string to see if it matches phone format
-						
 					var checkIfPhone = function(stringToCheck){ // implement charCodeAt, charAt (3) = "-", charAt(7) = "-", 
-						var stringToCheck = "404-857-8057";
+						var stringToCheck = "505-857-8057";
 						var checkChars1 = stringToCheck.substring(0, 3);
 						var checkChars2 = stringToCheck.substring(4, 7);
 						var checkChars3 = stringToCheck.substring(8, 12);
 						
-						if (stringToCheck.length == 0)
-					   		return false;
-					
-							if ((stringToCheck.charAt(3)!= "-") || (stringToCheck.charAt(7) != "-"))
+						
+						if (stringToCheck.length != 12)
+							return false;
+						if (stringToCheck.indexOf(".") != -1)
+							return false;
+						if ((stringToCheck.charAt(3)!= "-") || (stringToCheck.charAt(7) != "-"))
 					   		return false;
 					
 					
@@ -37,20 +38,66 @@ var myFunctionLibrary = function() {
 					
 					checkIfPhone(stringToCheck);
 	};
-	return stringToCheck;
-};
+
+
+					// To check string to see if it matches email format
+					
+							//check occurence of "@" = 1
+							// check occurence of "." = 1
+							// "@" must come first before "."
+							
+							
+			
+var checkIfEmail = function (myString){
 
 
 
+var myString = "ejayefe.gmail@com";
+var foundAtPosition = 0;
+var atCount = 0;
+var foundPeriodPosition = 0;
+var periodCount = 0;
 
 
+            while (foundAtPosition != -1)
+{
+            foundAtPosition = myString.indexOf("@",foundAtPosition);
+            if (foundAtPosition != -1)
+                                        {
+                                            atCount++;
+                                            foundAtPosition++;
 
+                                        }
+                while (foundPeriodPosition != -1)
+{
+            foundPeriodPosition = myString.indexOf(".",foundPeriodPosition);
+            if (foundPeriodPosition != -1)
+                                        {
+                                            periodCount++;
+                                            foundPeriodPosition++;
 
-
-
-
-
-
+                                        }
+        
+}                           
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+        
+}
+     if (((atCount != 1) || (periodCount != 1))||((myString.indexOf("@")) > (myString.indexOf(".")))){
+    return false;}
+   else 
+   {return true;}
+   
+      
+            
+}
+console.log (checkIfEmail(myString));
 
 
 
